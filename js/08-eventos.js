6f3987918db9744deefabb99520ea5ba18d05d08
Izabelle Santos <izabelle.sdn@gmail.com>
2022-04-13 16:59:36 -0300

    eventos

---
+++ js/08-eventos.js
@@ -0,0 +1,24 @@
+/* exemplo 1 */
+const exemplo01 = document.querySelector("#subtitulo-exemplo01");
+const msg = document.querySelector("#mensagem");
+
+// monitor/ouvinte de evento
+exemplo01.addEventListener("click", function(){
+    msg.textContent = "olá!";
+    exemplo01.style.color = "red";
+});
+
+exemplo01.addEventListener("dblclick", function(){
+    msg.textContent = "";
+    exemplo01.style.color = "blue";
+});
+
+//mouseover, mouseout, click, dblclick
+
+/* exemplo 2: modo noturno */
+const botao = document.querySelector("#noturno");
+const pagina = document.querySelector("body");
+
+botao.addEventListener("click", function(){
+    pagina.classList.toggle("modo-noturno");
+});
